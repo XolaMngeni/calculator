68d1d83ecb1d0e373796445d13eb2a84b03ebbc5
Xola Mngeni <xolamngeni@gamil.com>
2021-03-28 15:42:13 +0200

    First commit

---
+++ src/za/ac/cput/calculator/Division.java
@@ -0,0 +1,14 @@
+package za.ac.cput.calculator;
+
+public class Division {
+    /**
+     *
+     * returns result of integer division
+     *
+     */
+    public static int integerDivision(int numerator, int denominator){
+        if (denominator < 1) return 0;
+        return numerator / denominator;
+    }
+
+}
